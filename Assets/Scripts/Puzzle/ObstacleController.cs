9c6e430a59b951aa4e3942bb75932e0426644446
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-31 20:46:48 +0900

    [Add] Add PuzzleManager.cs & ObstacleController

---
+++ Assets/Scripts/Puzzle/ObstacleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    [SerializeField] private string[] targetKeys;
+    private bool[] isSwitchOn;
+
+    private void Awake()
+    {
+        isSwitchOn = new bool[targetKeys.Length];
+    }
+
+    private void Start()
+    {
+        foreach (string targetKey in targetKeys)
+        {
+            if (PuzzleManager.Instance.CheckDictionary(targetKey))
+            {
+                PuzzleManager.Instance.GetSwitch(targetKey).switchEvent += ApplySwitch;
+            }
+        }
+    }
+
+    protected virtual void ApplySwitch(string key, bool isSolved)
+    {
+        for (int i = 0; i < targetKeys.Length; i++)
+        {
+            if (targetKeys[i] == key)
+            {
+                isSwitchOn[i] = isSolved;
+                break;
+            }
+        }
+
+        Debug.Log(key + isSolved);
+    }
+}
