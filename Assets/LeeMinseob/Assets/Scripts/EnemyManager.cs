68bed97b77304f43e936cb0281409047fc911019
Yggfyhe <alstjqdl1@naver.com>
2024-10-31 21:20:03 +0900

    [Fix] 싱글톤 상속변경

--- Assets/LeeMinseob/Assets/Scripts/EnemyManager.cs
+++ Assets/LeeMinseob/Assets/Scripts/EnemyManager.cs
@@ -1,23 +1,9 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class EnemyManager : Singleton<EnemyManager>
 {
-    public static EnemyManager Instance { get; private set; }
     private List<Enemy> enemies = new List<Enemy>();
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
     public void RegisterEnemy(Enemy enemy)
     {
