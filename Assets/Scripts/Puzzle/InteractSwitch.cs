9e37cace2efa9fe80c73ca7340f2269c5db4efba
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-02 22:33:15 +0900

    [Add] Add Interact Switch

---
+++ Assets/Scripts/Puzzle/InteractSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class InteractSwitch : MonoBehaviour, IInteractable
+{
+    [SerializeField] private PuzzleSwitchCell cell;
+
+    private void Awake()
+    {
+        PuzzleManager.Instance.AddPuzzleSwitch(cell);
+    }
+
+    public string GetInteractPrompt()
+    {
+        return "E: 누르기";
+    }
+
+    public void OnInteract()
+    {
+        if (cell.state)
+            PuzzleManager.Instance.SetPuzzleSwitchState(cell.key, false);
+
+        else
+            PuzzleManager.Instance.SetPuzzleSwitchState(cell.key, true);
+    }
+}
