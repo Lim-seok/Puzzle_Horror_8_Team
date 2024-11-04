b021f86142a1dd7481b787f4c850bfa1e0448868
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-05 00:04:03 +0900

    [Add] Add ItemHoldChecker

---
+++ Assets/Scripts/Puzzle/ItemHoldChecker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ItemHoldChecker : MonoBehaviour
+{
+    [SerializeField] private PuzzleSwitchCell cell;
+
+    private void Awake()
+    {
+        PuzzleManager.Instance.AddPuzzleSwitch(cell);
+    }
+
+    private void Start()
+    {
+        CharacterManager.Instance.Player.interaction.OnHoldEvent += OnItemHold;
+    }
+
+    protected virtual void OnItemHold(bool isHolding)
+    {
+        PuzzleManager.Instance.SetPuzzleSwitchState(cell.key, isHolding);
+    }
+}
