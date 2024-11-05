8b35830be77057ebd6e21c2adf64d599dd77cec9
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-05 15:55:31 +0900

    [Add] Add Level System

---
+++ Assets/Scripts/Manager/LevelController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : Singleton<LevelController>
+{
+    [SerializeField] private string[] LevelArray;
+    private int currentLevel;
+
+    private void Start()
+    {
+        currentLevel = 0;
+    }
+
+    public void LoadLevel(int level)
+    {
+        currentLevel = level;
+        SceneManager.LoadScene(level);
+    }
+
+    public void NextLevel()
+    {
+        currentLevel++;
+        SceneManager.LoadScene(currentLevel);
+    }
+}
---
+++ Assets/Scripts/Object/ClearPortal.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ClearPortal : MonoBehaviour
+{
+    [SerializeField] private LayerMask targetLayers;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((targetLayers & (1 << other.gameObject.layer)) != 0)
+        {
+            LevelController.Instance.NextLevel();
+        }
+    }
+}
