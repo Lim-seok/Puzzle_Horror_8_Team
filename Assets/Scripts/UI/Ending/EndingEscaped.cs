160905e5c3d647ec705d8dad706b29c4d9f482ae
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-06 17:59:55 +0900

    [Update] 탈출 엔딩 연출 변경

--- Assets/Scripts/UI/Ending/EndingEscaped.cs
+++ Assets/Scripts/UI/Ending/EndingEscaped.cs
@@ -6,6 +6,11 @@ public class EndingEscaped : EndingTrapped
     [SerializeField] protected float raiseStartTime;
     [SerializeField] protected float raiseEndTime;
 
+    [SerializeField] protected GameObject fadeScreen2;
+    protected ScreenFader? fader2;
+    [SerializeField] protected float fadeStartTime2;
+    [SerializeField] protected float fadeEndTime2;
+
     public override void OnEndingTrigger()
     {
         GameObject targetObj = Instantiate(fadeScreen, targetParent);
@@ -22,7 +27,18 @@ public class EndingEscaped : EndingTrapped
         raiser.InitailizeRaiser(true);
         Invoke("CallRaiseUp", raiseStartTime);
 
+        Invoke("CallFadeIn2", fadeStartTime2);
+
         Invoke("GotoTitle", endTime);
+    }
+
+    private void CallFadeIn2()
+    {
+        GameObject targetObj2 = Instantiate(fadeScreen2, targetParent);
+        fader2 = targetObj2.GetComponent<ScreenFader>();
+
+        fader2.SetTime(fadeEndTime2);
+        fader2.FadeIn();
     }
 
     private void CallRaiseUp()
