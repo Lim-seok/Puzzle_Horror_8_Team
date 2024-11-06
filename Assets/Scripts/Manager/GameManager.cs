f6b92544bce01d7f2e3b2590c8224aa842f52e24
geuneda <whdrmsvjzl@naver.com>
2024-11-06 15:10:40 +0900

    feat: 파괴가능한 제네릭 싱글톤 추가 및 적용

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : SceneSingleton<GameManager>
 {
     public ButtonController[] buttons;
     private int activeButtonCount = 5;
