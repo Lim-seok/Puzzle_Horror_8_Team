5a45fa09f8bd9d278f7e0530e15e92502c3bc1c1
geuneda <whdrmsvjzl@naver.com>
2024-11-06 14:15:38 +0900

    chore: 코드 정리 및 리팩토링

--- Assets/Scripts/Manager/LevelController.cs
+++ Assets/Scripts/Manager/LevelController.cs
@@ -1,28 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class LevelController : Singleton<LevelController>
 {
-    [SerializeField] private string[] LevelArray;
     public int currentLevel;
-
-    private void Start()
-    {
-        LoadLevel();
-    }
-
-    public void LoadLevel()
-    {
-        SaveData saveData = SaveLoadManager.Instance?.LoadGame(SaveLoadManager.Instance.currentSlotIndex);
-        currentLevel = saveData.level;
-        SceneManager.LoadScene(LevelArray[currentLevel]);
-    }
 
     public void NextLevel()
     {
         currentLevel++;
+        SaveLoadManager.Instance.SaveCurrentProgress(currentLevel);
         SceneManager.LoadScene($"Level{currentLevel}");
-
     }
 }
